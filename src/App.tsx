77cf010a11503af8c5c9a4390d862f97cfed37eb
yeboka <mukanyerbolat@gmail.com>
2025-11-18 19:02:50 +0500

    test vibration

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,22 @@ import './App.css'
 import Container from './components/Container'
 
 function App() {
+  const handlePlayClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    console.log('Play button clicked', event.target)
+    navigator.vibrate(200)
+  }
+
   return (
     <div className='min-h-screen flex'>
-      <Container className='p-5 flex flex-col gap-5 rounded-2xl'>
+      <Container className='flex flex-col gap-5'>
         <div className='w-[300px] h-[300px] mx-auto border-8 border-white rounded-2xl'>
-          <img src="./images/currents-cover.jpg" alt="album cover" className='w-full max-h-[300px] rounded-[8px]' />
+          <img src='./images/currents-cover.jpg' alt='album cover' className='w-full max-h-[300px] rounded-[8px]' />
         </div>
         <div className='w-[300px] h-[300px] bg-blue-400 rounded-full mx-auto my-auto flex'>
-          <div className='w-[150px] h-[150px] border border-gray-500 rounded-full mx-auto my-auto'></div>
+          <button
+            className='w-[150px] h-[150px] border border-gray-500 rounded-full mx-auto my-auto'
+            onClick={handlePlayClick}
+          ></button>
         </div>
       </Container>
     </div>
--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -2,8 +2,14 @@ import { cn } from '@/lib/utils'
 import type { FC, ReactNode } from 'react'
 
 const Container: FC<{ className?: string; children: ReactNode }> = ({ className, children }) => {
+  const useAgent = typeof navigator !== 'undefined' ? navigator.userAgent : null
+  const isMobile = useAgent ? /Mobi|Android|iPhone/i.test(useAgent) : false
+  console.log('isMobile:', isMobile)
+  console.log('userAgent:', useAgent)
   return (
-    <div className={cn('max-w-[400px] max-h-[840px] w-full h-auto mx-auto my-auto bg-red-400', className)}>
+    <div
+      className={cn('w-full h-screen mx-auto my-auto bg-red-400 p-5', !isMobile && 'max-w-[390px] max-h-[700px] rounded-2xl', className)}
+    >
       {children}
     </div>
   )
