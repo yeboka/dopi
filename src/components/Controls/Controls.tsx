edb095c985969be5723f3bfdec25be164744faf9
yeboka <mukanyerbolat@gmail.com>
2025-12-02 23:50:26 +0500

    wheel scroll v1

--- src/components/Controls/Controls.tsx
+++ src/components/Controls/Controls.tsx
@@ -1,15 +1,54 @@
+import { Directions } from '@/shared/constants'
+import { useEffect, useState } from 'react'
+export type Point = { x: number; y: number }
 export const Controls = () => {
-  const handlePlayClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    console.log('Play button clicked', event.target)
-    navigator.vibrate(20)
+  const [isMouseDown, setIsMouseDown] = useState(false)
+  const [direction, setDirection] = useState(Directions.FORWARD)
+  const [a, setA] = useState<Point>({ x: 0, y: 0 })
+  const [b, setB] = useState<Point>({ x: 0, y: 0 })
+  const [c, setC] = useState<Point>({ x: 0, y: 0 })
+
+  // const handlePlayClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+  //   navigator.vibrate(20)
+  // }
+
+  const handleMouseMove = (event: React.MouseEvent<HTMLDivElement>) => {
+    if (!isMouseDown) return
+
+    setA(b)
+    setB(c)
+    setC({ x: event.clientX, y: event.clientY })
+
+    const dir = calculateDirection(a, b, c)
+    console.log(event.clientX, event.clientY, dir, a, b, c)
+    setDirection(dir)
+  }
+
+  const calculateDirection = (a: Point, b: Point, c: Point) => {
+    const u: Point = { x: b.x - a.x, y: b.y - a.y }
+    const v: Point = { x: c.x - b.x, y: c.y - b.y }
+
+    const uv = u.x * v.y - u.y * v.x
+
+    if (uv <= 0) {
+      return Directions.FORWARD
+    } else {
+      return Directions.BACKWARD
+    }
   }
 
   return (
-    <div className='flex-1 flex'>
-      <div className='w-full aspect-square bg-white rounded-full mx-auto my-auto flex'>
+    <div className='flex-1 flex flex-col'>
+      {direction ? 'backward' : 'forward'}
+      <div
+        onMouseDown={() => setIsMouseDown(true)}
+        onMouseUp={() => setIsMouseDown(false)}
+        onMouseMove={handleMouseMove}
+        className='w-[300px] aspect-square bg-white rounded-full mx-auto my-auto flex'
+      >
         <button
-          className='w-[150px] h-[150px]  border border-gray-500 rounded-full mx-auto my-auto'
-          onClick={handlePlayClick}
+          className='w-[125px] aspect-square border border-gray-500 rounded-full mx-auto my-auto'
+          // onClick={handlePlayClick}
         ></button>
       </div>
     </div>
--- src/components/Screen/Screen.tsx
+++ src/components/Screen/Screen.tsx
@@ -1,8 +1,24 @@
 export const Screen = () => {
+  const items = [
+    { id: 1, name: 'Cover FLow' },
+    { id: 1, name: 'Music' },
+    { id: 1, name: 'Settings' },
+    { id: 1, name: 'About' },
+    { id: 1, name: 'Sign in' }
+  ]
+
   return (
-    <div className='flex-1 flex'>
-      <div className='w-full aspect-square mx-auto my-auto p-2 bg-white border-white rounded-[10px]'>
-        <img src='./images/currents-cover.jpg' alt='album cover' className='' />
+    <div className='flex-1'>
+      <div className='w-full aspect-square mx-auto p-2 bg-white border-white rounded-[10px]'>
+        <div className='w-full h-full bg-gray-200 flex flex-col'>
+          <ul>
+            {items.map(item => (
+              <button key={item.id} className='p-4 border-b border-gray-300 focus:bg-blue-400 w-full text-left' data-menu-item>
+                {item.name}
+              </button>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   )
