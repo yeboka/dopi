72cefcfdbc32641e7202da9b148aac5ed9505b3c
yeboka <mukanyerbolat@gmail.com>
2025-12-03 14:34:07 +0500

    v3

--- src/components/Controls/Controls.tsx
+++ src/components/Controls/Controls.tsx
@@ -1,10 +1,12 @@
 import { Directions } from '@/shared/constants'
-import { useMemo, useState } from 'react'
+import React, { useCallback, useEffect, useMemo, useState } from 'react'
+import { throttle } from 'lodash'
+
 export type Point = { x: number; y: number }
 export const Controls = () => {
   const [isMouseDown, setIsMouseDown] = useState(false)
   const [direction, setDirection] = useState(Directions.FORWARD)
-  const [a, setA] = useState<number>(0)
+  const [count, setCount] = useState(0)
   // const [b, setB] = useState<Point>({ x: 0, y: 0 })
   // const [c, setC] = useState<Point>({ x: 0, y: 0 })
 
@@ -23,30 +25,56 @@ export const Controls = () => {
     }
   }, [])
 
-  const handleMouseMove = (event: React.MouseEvent<HTMLDivElement>) => {
-    if (!isMouseDown) return
+  const handleClickWheelScroll = useCallback(() => {
+    let prevTheta = 0
+    return (event: React.MouseEvent<HTMLDivElement>) => {
+      if (!isMouseDown) return
 
-    const result = Math.atan(
-      (event.clientX - centerPoint.x) / (event.clientY - centerPoint.y)
-    )
-    console.log(result)
-    if (result < a) {
-      setDirection(Directions.FORWARD)
-    } else if (result > a) {
-      setDirection(Directions.BACKWARD)
+      const thetaCurrent = Math.atan2(
+        event.clientY - centerPoint.y,
+        event.clientX - centerPoint.x
+      )
+      let deltaTheta = thetaCurrent - prevTheta
+
+      deltaTheta = deltaTheta > Math.PI ? deltaTheta - 2 * Math.PI : deltaTheta
+      deltaTheta = deltaTheta < -Math.PI ? deltaTheta + 2 * Math.PI : deltaTheta
+
+      if (Math.abs(deltaTheta) > 0.01) {
+        //avoiding jitter
+        console.log(deltaTheta)
+
+        setDirection(deltaTheta > 0 ? Directions.FORWARD : Directions.BACKWARD)
+        if (deltaTheta > 0) {
+          setCount(prev => prev + 1)
+        } else {
+          setCount(prev => prev - 1)
+        }
+      }
+      prevTheta = thetaCurrent
     }
-    setA(result)
-  }
+  }, [isMouseDown, centerPoint])
+
+  const throttledMouseMove = useMemo(
+    () => throttle(handleClickWheelScroll(), 100),
+    [handleClickWheelScroll]
+  )
+
+  useEffect(() => {
+    return () => {
+      throttledMouseMove.cancel()
+    }
+  }, [throttledMouseMove])
 
   return (
-    <div className='flex-1 flex flex-col'>
-      {direction ? 'backward' : 'forward'}
+    <div className='flex-1 flex flex-col items-center'>
+      {direction ? 'backward' : 'forward'} {count}
       <div
         onMouseDown={() => setIsMouseDown(true)}
         onMouseUp={() => setIsMouseDown(false)}
         onTouchStart={() => setIsMouseDown(true)}
         onTouchEnd={() => setIsMouseDown(false)}
-        onMouseMove={handleMouseMove}
+        onMouseMove={throttledMouseMove}
+        // onTouchMove={throttledTouchMove}
         className='w-[300px] aspect-square bg-white rounded-full mx-auto my-auto flex'
         id='clickwheel'
       >
