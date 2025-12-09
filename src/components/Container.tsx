12876dbc4e8df64e95b09fe5a48809dedbfc8430
yeboka <mukanyerbolat@gmail.com>
2025-12-09 17:37:14 +0500

    clickwheel v6

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -12,9 +12,9 @@ const Container: FC<{ className?: string; children: ReactNode }> = ({
   return (
     <div
       className={cn(
-        'w-full mx-auto p-9 flex-1 flex flex-col inset-shadow-lg  relative overflow-clip bg-wheel bg-center',
+        'w-full mx-auto p-9 flex-1 flex flex-col inset-shadow-lg  relative overflow-hidden bg-wheel bg-center',
         !isMobile &&
-          'max-w-[420px] rounded-[46px] shadow-soft-xl aspect-[1/2] w-full h-full',
+          'max-w-[420px] rounded-[46px] shadow-soft-xl aspect-[1/2] w-full h-full overflow-clip',
         className
       )}
       {...props}
